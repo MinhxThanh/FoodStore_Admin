72b5e7259e2ca2c89b86e96ca81a4f8b8df18e7e
MinhxThanh <lethanh.000113@gmail.com>
2023-02-19 14:17:54 +0700

    Login and register admin

---
+++ js/admin-app.js
@@ -0,0 +1,80 @@
+const app = angular.module('adminApp', ['ngRoute', 'ngAnimate'])
+app.controller('mainController', function($scope, $location, $window){
+    $scope.username = sessionStorage.getItem('username')
+    $scope.avatar = sessionStorage.getItem('avatar')
+
+    $scope.logout = function(){
+        sessionStorage.removeItem('username')
+        $scope.username = ''
+        $window.location.reload();
+        location.href = "index.html"
+    }
+
+    $scope.isActive = function(url){
+       return url === $location.path()
+    }
+})
+
+app.config(function ($routeProvider){
+    $routeProvider
+        .when("/home", {
+            templateUrl: "home/index.html?" + Math.random(),
+            controller: "home-controller"
+        })
+        .when("/security/login", {
+            templateUrl: "security/login.html?" + Math.random(),
+            controller: "security-controller"
+        })
+        .when("/security/register", {
+            templateUrl: "security/register.html?" + Math.random(),
+            controller: "security-controller"
+        })
+        .when("/security/forgot-password", {
+            templateUrl: "security/forgot-password.html?" + Math.random(),
+            controller: "security-controller"
+        })
+        .otherwise({
+            templateUrl: "/404.html?" + Math.random(),
+            controller: 'home-controller'
+        })
+})
+
+app.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
+
+app.factory('sessionInjector', function() {  
+    var sessionInjector = {
+        request: function(config) {
+            config.headers['Authorization'] = 'Bearer '+ sessionStorage.getItem('accessToken');
+            return config;
+        }
+    };
+    return sessionInjector;
+});
+
+var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+
+app.directive("compareTo", compareTo);
+
+// app.config(['$httpProvider', function($httpProvider) {
+//     if(localStorage.getItem('accessToken') != null)
+//         $httpProvider.interceptors.push('sessionInjector');
+// }]);
---
+++ security/security-controller.js
@@ -0,0 +1,59 @@
+app.controller('security-controller', function ($scope, $http, $window) {
+    $scope.message = ""
+    $scope.error = ""
+    $scope.login = {
+        loginInfo: {},
+        loginAccount(){
+            console.log("user", this.loginInfo)
+            $http.post(`http://localhost:8080/rest/auth/login`, this.loginInfo).then(async resp =>{
+                sessionStorage.setItem('email', resp.data.email)
+                sessionStorage.setItem('accessToken', resp.data.accessToken)
+                sessionStorage.setItem('username', resp.data.username)
+                sessionStorage.setItem('avatar', resp.data.avatar)
+                $scope.message = "Login success!!"
+                await $scope.sleep(1000)
+                $window.location.reload();
+            }).catch(err => {
+                console.log("Err", err)
+                $scope.error = "Wrong email or password!"
+            })
+        }
+    }
+
+    $scope.register = {
+        isEmail: false,
+        isUsername: false,
+        item: {},
+        registerController(){
+            $http.post(`http://localhost:8080/rest/auth/register`, this.item).then(resp =>{
+                $scope.message = "Register successfully!"
+            }).catch(err => {
+                console.log("Err", err)
+                $scope.error = "Please enter correctly!"
+            })
+        },
+        searchEmail(){
+            $http.get(`http://localhost:8080/rest/user/searchByEmailOrUsername/${this.item.email}`).then(resp =>{
+                if (resp.data.email != null) {
+                    this.isEmail = true
+                } else {
+                    this.isEmail = false
+                }
+            }).catch(err => $scope.error = "Please enter email correctly!")
+        },
+        searchUsername(){
+            $http.get(`http://localhost:8080/rest/user/searchByEmailOrUsername/${this.item.username}`).then(resp =>{
+                if (resp.data.email != null) {
+                    this.isUsername = true
+                } else {
+                    this.isUsername = false
+                }
+            }).catch(err => $scope.error = "Please enter username correctly!")
+        }
+    }
+
+
+    $scope.sleep = function(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+})
